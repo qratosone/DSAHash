67fff5b2e80396b2919f8f712cc5a342a3b36cd3
The space of Qratosone <qratosone@live.com>
2015-12-11 01:32:24 +0800

    Add PA3

--- DSAHash/DSAHash.cpp
+++ DSAHash/DSAHash.cpp
@@ -5,24 +5,34 @@
 #include <iostream>
 #include <string>
 #include "Hash.h"
+#include "MyVector.h"
 using namespace std;
+
 int main()
 {
 	string input;
 	int n;
 	cin >> n;
+	MyVector<string> v(n);
+	int hashCode[100000] = { 0 };
+	int count = 0;
 	HashTable hash(120000);
 	for (int i = 0; i < n; i++)
 	{
 		cin >> input;
-		if (hash.searchFor(input))
-		{
-			cout << input << endl;
+		if (hash.insert(input)==2) {
+			int code = hash.hash(input);
+			if (hashCode[code]==0)
+			{
+				v.insert(v.length(), input);
+				count++;
+				hashCode[code] = 1;
+			}
 		}
-		else
-		{
-			hash.insert(input);
-		}
+	}
+	for (int i = 0; i < v.length(); i++)
+	{
+		cout << v[i] << endl;
 	}
     return 0;
 }
